6b4f9361dd4c1c44e4ecc208ec8e6674a7c7c5ba
Rowland Rowland <rowlandr71@gmail.com>
2022-09-18 18:48:48 +0100

    Added the correct naming header to .java file

--- src/main/java/com/example/CICD_project/CiCdProjectApplication.java
+++ src/main/java/com/example/CICD_project/CiCdProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CiCdprojectApplication {
+public class CiCdProjectApplication {
 
 
 	@RequestMapping("/")
