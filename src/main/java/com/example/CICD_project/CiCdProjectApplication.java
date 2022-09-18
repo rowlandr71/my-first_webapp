2237a1d632613a8ee6dba5ad7c89f7886440dd64
Rowland Rowland <rowlandr71@gmail.com>
2022-09-18 18:08:22 +0100

    added a new web feature to display hello world

--- src/main/java/com/example/CICD_project/CiCdProjectApplication.java
+++ src/main/java/com/example/CICD_project/CiCdProjectApplication.java
@@ -1,10 +1,19 @@
 package com.example.CICD_project;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CiCdProjectApplication {
+public class CiCdprojectApplication {
+
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdProjectApplication.class, args);
